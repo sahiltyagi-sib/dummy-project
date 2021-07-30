c153cbfbaf7c0bbe8d8e460088289d98bc74f343
Sahil Tyagi <sahil.tyagi@sendinblue.com>
2021-07-30 19:05:17 +0530

    added basic routes for put and delete too

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,12 @@ router.post("/", (req, res) => {
   res.send(req.body);
 });
 
+router.put("/", (req, res) => {
+  res.send("Updated some data");
+});
+
+router.delete("/", (req, res) => {
+  res.send("Deleted some data");
+});
+
 module.exports = router;
