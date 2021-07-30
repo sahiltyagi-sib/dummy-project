d5cf901d7d2aebab83f0236b501a02b1c2d1ad7f
Sahil Tyagi <sahil.tyagi@sendinblue.com>
2021-07-30 16:52:47 +0530

    Tried basic routing
    installed nodemon as a dev dependancy and now using it to run the app. added a new script to use it. created a routes folder and added an index.js file in it created routes 1 for get and 2nd for post request at / home page route. Tried using body-parser to parse data coming in incoming post request but it was giving warning body-parser is deprecated. Read about alternative and found that now we can use express.json() to parse json data in body so using it now

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
+const routes = require("./routes");
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+app.use(express.json());
+app.use("/", routes);
 
 app.listen(3000, () => {
   console.log("server running on 3000");
