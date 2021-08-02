e4c8121dd618a36bf1b9e72e918ff29b4216b445
Sahil Tyagi <sahil.tyagi@sendinblue.com>
2021-08-02 18:45:34 +0530

    User schema and signup
    Added user schema using mongoose. User schema consist of front_name, last_name, email, password. Password is encrypted using bcryptjs library. A new route has been created which is used to signup user. The data is stored on mongodb atlas. A jwt token is created and sent back to the user.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
+const User = require("../model/user");
+const bcrypt = require("bcryptjs");
 
-router.get("/", (req, res) => {
-  res.send("Homepage");
+router.post("/register", async (req, res) => {
+  try {
+    const { first_name, last_name, email, password } = req.body;
+
+    if (!(first_name && last_name && email && password)) {
+      res.status(400).send("All inputs are required");
+    }
+
+    const oldUser = await User.findOne({ email });
+    if (oldUser) {
+      return res.status(409).send("User already exists");
+    }
+
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      first_name,
+      last_name,
+      email: email.toLowerCase(),
+      password: encryptedPassword,
+    });
+
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.TOKEN_KEY,
+      { expiresIn: "2h" }
+    );
+
+    res.status(201).json(token);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.post("/login", (req, res) => {
