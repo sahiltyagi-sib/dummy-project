ee570537305b6efcb2334f2bc99b5a557dde460f
Sahil Tyagi <sahil.tyagi@sendinblue.com>
2021-08-03 17:55:52 +0530

    Added morgan
    Now morgan logs out the http requests the express app handles

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 const routes = require("./routes");
 require("dotenv").config();
 require("./config/database").connect();
 
+app.use(morgan("dev"));
 app.use(express.json());
 app.use("/", routes);
 
